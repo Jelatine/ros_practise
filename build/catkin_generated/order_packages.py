5995597e0180889d551c5e20b97425fad8c5a8dc
Lee <429572807@qq.com>
2018-08-12 00:38:47 +0800

    Init Ros workspace

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/lee/ros_workspaces/ros_practise/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/lee/ros_workspaces/catkin_ws/devel;/home/lee/ros_workspaces/sample2edition/devel;/opt/ros/lunar".split(';') if "/home/lee/ros_workspaces/catkin_ws/devel;/home/lee/ros_workspaces/sample2edition/devel;/opt/ros/lunar" != "" else []
